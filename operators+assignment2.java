a045819f79008bbdf515297de8079160f25a213c
priyanshiiguptaaa <152763463+priyanshiiguptaaa@users.noreply.github.com>
2024-01-21 16:34:10 +0530

    Create operators+assignment2.java

---
+++ operators+assignment2.java
@@ -0,0 +1,12 @@
+// types of operators in java
+/* 
+ 1. arithmetic - unary (post increment and pre increment) and binary 
+ 2. relational
+ 3. logical 
+ 4. bitwise 
+ 5. assignment 
+ */
+
+ // REFER TO OPERATOR PRECEDENCE TABLE 
+
+ // assignment 2 - no codes only output based questions 
